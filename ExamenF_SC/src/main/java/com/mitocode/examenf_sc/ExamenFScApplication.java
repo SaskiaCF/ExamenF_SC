c5f80f68d469055ca454b9cfc7ec74026b0fa51b
SaskiaCornejo <saskia.cornejo@teamsoft.com.pe>
2025-02-19 18:10:05 -0500

    update

---
+++ ExamenF_SC/src/main/java/com/mitocode/examenf_sc/ExamenFScApplication.java
@@ -0,0 +1,13 @@
+package com.mitocode.examenf_sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFScApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFScApplication.class, args);
+	}
+
+}
---
+++ ExamenF_SC/src/test/java/com/mitocode/examenf_sc/ExamenFScApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.examenf_sc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFScApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
